11c710fa2b5092578dcfb68544f50d8e4f4bab37
takuyaOba <49937871+takuyaOba@users.noreply.github.com>
2022-03-22 00:34:41 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by mt-square.toba on 2022/03/22.
+//  Copyright © 2022 takuyaOba. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
