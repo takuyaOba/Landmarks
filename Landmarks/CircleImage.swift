a06c73cbc611da5fdd4ce2870dc0ef1450480b96
takuyaOba <49937871+takuyaOba@users.noreply.github.com>
2022-03-22 00:42:35 +0900

    Initial Commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by mt-square.toba on 2022/03/22.
+//  Copyright © 2022 takuyaOba. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
